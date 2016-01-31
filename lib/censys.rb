0767aeeffffe49fd4fc7b3a1f29951c45fa47395
jcran <jcran@pentestify.com>
2016-01-31 09:45:13 -0800

    add data and search endpoints

--- lib/censys.rb
+++ lib/censys.rb
@@ -1,24 +1,67 @@
+require 'rest-client'
+# Working Python
+=begin
+import sys
+import json
+import requests
+
+res = requests.get(API_URL + "/data", auth=(UID, SECRET))
+if res.status_code != 200:
+    print "error occurred: %s" % res.json()["error"]
+    sys.exit(1)
+for name, series in res.json()["raw_series"].iteritems():
+    print series["name"], "was last updated at", series["latest_result"]["timestamp"]
+=end
+
 require_relative "censys/version"
 require "json"
 
 module Censys
-  class API
+  class Api
 
-    def initialize(uid=nil,secret=nil)
-      uri = "https://www.censys.io/api/v1"
+    def initialize(uid=nil,secret=nil, options={})
+      @uri = "https://www.censys.io/api/v1"
 
+      # TODO - allow proxy configuration here...
 
       unless uid && secret
         config_file = "#{File.dirname(__FILE__)}/../config/config.json"
         config = JSON.parse(File.open(config_file,"r").read)
-        uid = config["uid"]
-        secret = config["secret"]
+        @uid = config["uid"]
+        @secret = config["secret"]
       end
-      puts "UID #{uid}"
-      puts "SECRET #{secret}"
+
     end
+
+    def data
+      response = RestClient::Request.new(
+        :method => :get,
+        :url => "#{@uri}/data",
+        :user => @uid,
+        :password => @secret,
+        :headers => { :accept => :json, :content_type => :json }
+      ).execute
+    results = JSON.parse(response.to_str)
+    end
+
+    def search(keyword)
+      # "80.http.get.headers.server: Apache"
+      payload = {
+        "query": keyword
+      }
+
+      response = RestClient::Request.new(
+        :method => :post,
+        :url => "#{@uri}/search/certificates",
+        :user => @uid,
+        :password => @secret,
+        :headers => { :accept => :json, :content_type => :json },
+        :payload => payload.to_json
+      ).execute
+      results = JSON.parse(response.to_str)
+    end
+
   end
 end
 
-
-Censys::API.new
+x = Censys::Api.new.search("intrigue")
